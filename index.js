1e9dbc5605ec8ba06b4e27b12dd2a796992dda54
Pruthvi <146100300+pruthvishetty1789@users.noreply.github.com>
2025-10-31 11:21:59 +0530

    Comment out server listener and export app
    Comment out the app.listen function and export the app.

--- index.js
+++ index.js
@@ -90,6 +90,7 @@ app.delete("/api/bookings/:id", async (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+export default app;
